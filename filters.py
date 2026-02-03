df628cb01a20b55aea599336c1a611e37880666d
nestren <90449667+NESTREN@users.noreply.github.com>
2026-02-03 23:40:24 +0400

    фильтры дб

---
+++ filters.py
@@ -0,0 +1,12 @@
+﻿from aiogram.filters import Filter
+from aiogram.types import TelegramObject
+
+from db import Database
+
+
+class IsAdmin(Filter):
+    async def __call__(self, event: TelegramObject, db: Database) -> bool:
+        user = getattr(event, "from_user", None)
+        if not user:
+            return False
+        return await db.is_admin(user.id)
