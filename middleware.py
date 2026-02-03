b93818d8fc71c95134b85408d898b4c53c6f1372
nestren <90449667+NESTREN@users.noreply.github.com>
2026-02-03 23:40:55 +0400

    мидла

---
+++ middleware.py
@@ -0,0 +1,22 @@
+﻿from aiogram import BaseMiddleware
+
+from config import Config
+from db import Database
+
+
+class DbMiddleware(BaseMiddleware):
+    def __init__(self, db: Database) -> None:
+        self.db = db
+
+    async def __call__(self, handler, event, data):
+        data["db"] = self.db
+        return await handler(event, data)
+
+
+class ConfigMiddleware(BaseMiddleware):
+    def __init__(self, config: Config) -> None:
+        self.config = config
+
+    async def __call__(self, handler, event, data):
+        data["config"] = self.config
+        return await handler(event, data)
